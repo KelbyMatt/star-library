77b9028ee16232a5ac355f95dad9f81d77558419
KelbyMatt <kelbymatt14@gmail.com>
2025-10-11 15:04:19 +0100

    Restructured schemas file to correct an API recursion error when fetching authors

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,41 +1,47 @@
 from pydantic import BaseModel, ConfigDict
 from typing import List
 
-class AuthorBase(BaseModel):
-    name: str
-
-class AuthorCreate(AuthorBase):
-    pass
-
-
-class AuthorPublic(AuthorBase):
-    id: int
-    books: List['BookPublic'] = []
-
-    model_config = ConfigDict(from_attributes=True)
 
 class BookBase(BaseModel):
     title: str
 
-class BookCreate(BookBase):
-    author_id: int
-
-class BookPublic(BookBase):
-    id: int
-    author: Author
-
-    model_config = ConfigDict(from_attributes=True)
+class AuthorBase(BaseModel):
+    name: str
 
 class ReaderBase(BaseModel):
     name: str
 
+
+class BookCreate(BookBase):
+    author_id: int
+
+class AuthorCreate(AuthorBase):
+    pass
+
 class ReaderCreate(ReaderBase):
     pass
 
-class ReaderPublic(ReaderBase):
-    id: int
-    read_books: List[Book] = []
 
+class BookInAuthor(BookBase):
+    id: int
     model_config = ConfigDict(from_attributes=True)
 
-AuthorPublic.model_rebuild()
+class AuthorInBook(AuthorBase):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BookPublic(BookBase):
+    id: int
+    author: AuthorInBook
+    model_config = ConfigDict(from_attributes=True)
+
+class AuthorPublic(AuthorBase):
+    id: int
+    books: List[BookInAuthor] = []
+    model_config = ConfigDict(from_attributes=True)
+
+class ReaderPublic(ReaderBase):
+  
+    read_books: List[BookPublic] = []
+    model_config = ConfigDict(from_attributes=True)
