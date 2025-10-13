1ddef252075199b4a2ab78d8c35d401902cf13d6
KelbyMatt <kelbymatt14@gmail.com>
2025-10-13 13:29:24 +0100

    Cleaned up the setup process of database, seperating it from the main app startup

--- backend/manage.py
+++ backend/manage.py
@@ -1,8 +1,15 @@
+"""
+A simple utility script to set up the database.
+Run this once before starting the app for the first time.
+"""
+
+
 from app.database import engine
 from app.models import dbBase
 
 print("Attempting to create database tables...")
 
+# Line reads the table definitions from models.py and creates them.
 dbBase.metadata.create_all(bind=engine)
 
 print("Successfully created Database tables (if they didn't already exist).")
