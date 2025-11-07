4188adb4867e19b0ba7806d864e3bf1c9edf6c02
KelbyMatt <kelbymatt14@gmail.com>
2025-11-07 16:33:37 +0000

    SQLAlchemy models reformatted for clarity

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,59 +1,47 @@
-"""
-This file defines the SQLAlchemy database models.
-These classes are the Python representation of our database tables.
-"""
-
-from sqlalchemy import (Column, Integer, String, ForeignKey, Table)
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    Table
+)
 from sqlalchemy.orm import relationship, declarative_base
 
+# Create the base class for our models
 dbBase = declarative_base()
 
-# Association table for the many-to-many relationship
-# between books and readers.
+# Define the association table for the many-to-many relationship
 book_loans_table = Table(
     'book_loans',
     dbBase.metadata,
     Column('book_id', Integer, ForeignKey('books.id'), primary_key=True),
-    Column('reader_id', Integer, ForeignKey('readers.id', primary_key=True))
+    Column('reader_id', Integer, ForeignKey('readers.id'), primary_key=True)
 )
-
 
 class Author(dbBase):
     __tablename__ = "authors"
-
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, unique=True)
-
-    # One-to-many relationship from Author to Book
+    name = Column(String, unique=True, index=True)
     books = relationship("Book", back_populates="author")
-
-
 
 class Book(dbBase):
     __tablename__ = "books"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     author_id = Column(Integer, ForeignKey("authors.id"))
-
-    # Many-to-one link back Author
     author = relationship("Author", back_populates="books")
-
-    # Many-to-many relationship to Readers
-    readers = relationship("Reader",
-        secondary = book_loans_table,
-        back_populates = "read_books"
+    readers = relationship(
+        "Reader",
+        secondary=book_loans_table,
+        back_populates="read_books"
     )
-
 
 class Reader(dbBase):
     __tablename__ = "readers"
-
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-
-    # Many-to-many link back to Books
-    read_books = relationship("Book",
+    read_books = relationship(
+        "Book",
         secondary=book_loans_table,
         back_populates="readers"
     )
